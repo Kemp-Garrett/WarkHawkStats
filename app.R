71d78a0d734f51c692805dece41aa6e4e5d140ce
GK-Sabermetrics <kempgarrettm@gmail.com>
2026-09-24 21:36:13 -0400

    t

--- app.R
+++ app.R
@@ -4,6 +4,7 @@
 library(shiny)
 library(bslib)
 library(shinymanager)
+library(rsconnect)
 
 # Placeholder page for sections that haven't been built yet
 section_placeholder <- function(title, blurb) {
